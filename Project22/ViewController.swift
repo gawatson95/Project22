0719a4f1846cac9fdf18ddc293bb822ffda901d6
Grant Watson <gaw922@gmail.com>
2022-10-24 12:07:12 -0500

    Initial Commit

---
+++ Project22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project22
+//
+//  Created by Grant Watson on 10/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
